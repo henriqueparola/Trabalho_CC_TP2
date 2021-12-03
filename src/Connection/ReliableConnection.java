f9d44cc045a329201c1e3bea7682c3f9f5906d1f
jpdiasfernandes <jpdiasfernandes13@gmail.com>
2021-12-03 22:56:44 +0000

    implement reliable stop-wait send
    - Created a timeout for only peer packets

--- src/Connection/ReliableConnection.java
+++ src/Connection/ReliableConnection.java
@@ -7,6 +7,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.concurrent.*;
 
 public class ReliableConnection {
     private DatagramSocket socket;
@@ -25,31 +26,77 @@ public class ReliableConnection {
         // Se data for maior que MTU é necessário fazer a divisão por vários pacotes
         // Como agora estou a fazer stop and wait preciso de receber um ack para cada pacote enviado
        DataInputStream  dis = new DataInputStream(new BufferedInputStream(new ByteArrayInputStream(data)));
-
-
         byte dataOut[] = new byte[MTU];
         // Recebe-se um Frame e não dados
-        byte dataIn[] = new byte[MTU + 4];
-        byte frameOut[];
+        ConnectionFrame frameIn;
+        // Para saber se se pode saltar o ciclo de tentativas
+        boolean received = false;
+        // Máximas tentativas de timeout
+        final int maxTries = 5;
+        // para saber o tamanho lido da stream
         int size = 0;
 
 
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<ConnectionFrame> future = executor.submit(new Callable<ConnectionFrame>() {
+            @Override
+            public ConnectionFrame call() throws Exception {
+                return rdtRcvPckt();
+            }
+        });
+
         while ((size = dis.read(dataOut)) != -1) {
-            ConnectionFrame outFrame = new ConnectionFrame(seq++, size, dataOut);
-            frameOut = outFrame.serialize();
-            DatagramPacket outPacket = new DatagramPacket(frameOut,
-                                                        frameOut.length,
-                                                        this.peerAddress,
-                                                        this.peerPort);
-            socket.send(outPacket);
+            udtSendPckt(size, dataOut, this.seq);
 
-            DatagramPacket inPacket = new DatagramPacket(dataIn, dataIn.length);
+            for (int i = 0; i < maxTries && received; i++) {
+                try {
+                    frameIn = future.get(1, TimeUnit.SECONDS);
+                    if (notCurrupt(frameIn) && isAck(frameIn, this.seq + 1)) {
+                        received = true;
+                        this.seq++;
+                    }
+                } catch (TimeoutException | InterruptedException | ExecutionException e) {
+                    future.cancel(true);
+                    udtSendPckt(size, dataOut, seq);
+                }
+            }
 
-            socket.receive(inPacket);
-            ConnectionFrame inFrame = ConnectionFrame.deserealize(inPacket.getData());
-            //Sei que recebi um ack, no entanto convém verificar se data é null ou se data size = 0;
-            //Como é stopwait não preciso de saber qual é o numero da seq
         }
+    }
+
+    private boolean sameRecipient(InetAddress address, int port) {
+        return address.equals(this.peerAddress) && this.peerPort == port;
+    }
+
+    private boolean isAck(ConnectionFrame frame, int seq) {
+        return frame.tag == seq && frame.dataLen == 0;
+    }
+
+    private boolean notCurrupt(ConnectionFrame frame) {
+        return true;
+    }
+
+    private void udtSendPckt(int size, byte[] data, int seq) throws IOException {
+        ConnectionFrame outFrame = new ConnectionFrame(seq, size, data);
+        byte[] frameOut;
+        frameOut = outFrame.serialize();
+        DatagramPacket outPacket = new DatagramPacket(frameOut,
+                                                    frameOut.length,
+                                                    this.peerAddress,
+                                                    this.peerPort);
+        socket.send(outPacket);
+    }
+
+    private ConnectionFrame rdtRcvPckt() throws IOException {
+        byte dataIn[] = new byte[ConnectionFrame.MTU];
+        DatagramPacket inPacket = new DatagramPacket(dataIn, dataIn.length);
+        socket.receive(inPacket);
+
+        while(!sameRecipient(inPacket.getAddress(), inPacket.getPort()))
+            socket.receive(inPacket);
+
+        ConnectionFrame inFrame = ConnectionFrame.deserealize(inPacket.getData());
+        return inFrame;
     }
 
     public byte[] receive() throws IOException {
@@ -64,7 +111,12 @@ public class ReliableConnection {
         while (flag) {
             socket.receive(inPacket);
             // Verificar a integridade do que se recebeu
+
             ConnectionFrame inFrame = ConnectionFrame.deserealize(inPacket.getData());
+
+            InetAddress senderAddress = inPacket.getAddress();
+            int senderPort = inPacket.getPort();
+
 
             if (inFrame.dataLen == 0) flag = false;
             else {
@@ -73,8 +125,9 @@ public class ReliableConnection {
                 byte []frameOut = outFrame.serialize();
                 DatagramPacket outPacket = new DatagramPacket(frameOut,
                                                             frameOut.length,
-                                                            this.peerAddress,
-                                                            this.peerPort);
+                                                            senderAddress,
+                                                            senderPort);
+                socket.send(outPacket);
             }
         }
         return baos.toByteArray();
