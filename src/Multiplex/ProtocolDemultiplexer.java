3b767d3ffe9cc5b709bbffe3f8fbdf4ed7a6a854
Parola <parolajc@hotmail.com>
2021-12-07 23:53:37 +0000

    add folderToSync to FileReply

--- src/Multiplex/ProtocolDemultiplexer.java
+++ src/Multiplex/ProtocolDemultiplexer.java
@@ -2,10 +2,12 @@ package Multiplex;
 
 import Client.FolderStruct;
 import Connection.ReliableConnection;
+import Server.FileReply;
 import Server.StructReply;
 
 import java.io.IOException;
 import java.net.SocketException;
+import java.nio.charset.StandardCharsets;
 
 public class ProtocolDemultiplexer implements Runnable {
     FolderStruct fd;
@@ -28,8 +30,16 @@ public class ProtocolDemultiplexer implements Runnable {
                     case 0x0:
                         Thread t = new Thread(new StructReply(rb.peerAddress,rb.peerPort,folderToSync));
                         t.start();
+                        break;
                     case 0x1:
-                        // TODO  File Reply
+                        Thread t2 = new Thread(new FileReply(
+                                rb.peerAddress,
+                                rb.peerPort,
+                                folderToSync,
+                                new String(pf.data, StandardCharsets.UTF_8)
+                        ));
+                        t2.start();
+                        break;
                 }
 
                 rb.socket.close();
