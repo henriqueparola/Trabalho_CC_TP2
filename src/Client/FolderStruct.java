626f6867d0e6e5a6dc356b18a661fa990f23f2c1
Parola <parolajc@hotmail.com>
2021-12-07 23:50:51 +0000

    add createFolderStruct method

--- src/Client/FolderStruct.java
+++ src/Client/FolderStruct.java
@@ -1,5 +1,6 @@
 package Client;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,49 +15,24 @@ import java.util.stream.Stream;
 public class FolderStruct {
     private Map<String, List<MetaData>> folder = new HashMap<>();
 
-    public void addList(String ip, List<MetaData> foldersFromIp){
-        folder.put(ip,foldersFromIp);
-    }
-
-    public Map<String,List<MetaData>> getStruct(){
-        // F encapsulamento
-        return folder;
-    }
-
-    public void sync(String folderToSync){
-        Path path = Paths.get("./" + folderToSync);
-
-        Stream<Path> walk = null;
-        try {
-            walk = Files.walk(path);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        List<Path> paths;
-        paths = walk.filter(Files::isRegularFile).collect(Collectors.toList());
-        List<MetaData> myMetaDataPaths = paths.stream().map(p -> getMetaData(p)).toList();
-
-        /* Objetivo: verificar se algum dos Paths enviados corresponde a um que já possuo */
-        /* TODO Ver se a forma melhor de fazer isto */
-        for(List<MetaData> metasFromAnoter: folder.values()){
-            for(MetaData metaDataFromAnother: metasFromAnoter){
-                for(MetaData myMetaData: myMetaDataPaths){
-                    if (myMetaData.equals(metaDataFromAnother)){
-                        folder.remove(metaDataFromAnother);
-                    }
+    public void createFolderStruct(String folderToSync) {
+        for (Map.Entry<String, List<MetaData>> entry : folder.entrySet()) {
+            for (MetaData metadata : entry.getValue()){
+                int lastSeparator = metadata.getFilePath().lastIndexOf("/");
+                if (lastSeparator != -1){
+                    String directoryName = metadata.getFilePath().substring(0,lastSeparator);
+                    File folderPath = new File(folderToSync + "/" + directoryName);
+                    folderPath.mkdirs();
                 }
             }
         }
     }
 
-    /* Repetição */
-    private MetaData getMetaData(Path path){
-        try {
-            BasicFileAttributes attr = Files.readAttributes(path, BasicFileAttributes.class);
-            return new MetaData(path.toString(),attr.size(),attr.creationTime().toMillis(),attr.lastModifiedTime().toMillis());
-        }catch (IOException e){
-        }
-        return null;
+    public void addList(String ip, List<MetaData> foldersFromIp){
+        folder.put(ip,foldersFromIp);
+    }
+
+    public Map<String,List<MetaData>> getStruct(){
+        return folder;
     }
 }
