04e6773380f555a8027ff43e0c785645d7091c10
MarcoEsperanca <marcoesperanca33@gmail.com>
2021-12-09 11:20:13 +0000

    Update security (3)

--- src/Security/CreatePassword.java
+++ src/Security/CreatePassword.java
@@ -3,7 +3,9 @@ package Security;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.InvalidKeyException;
 import java.util.Arrays;
@@ -12,8 +14,8 @@ import java.util.Arrays;
 public class CreatePassword {
         public static byte[] generateHMAC(String sharedKey) {
             try {
-                Mac hash = Mac.getInstance("HmacSHA256");
-                SecretKeySpec key = new SecretKeySpec(sharedKey.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+                Mac hash = Mac.getInstance("HmacSHA1");
+                SecretKeySpec key = new SecretKeySpec(sharedKey.getBytes(StandardCharsets.UTF_8), "HmacSHA1");
                 hash.init(key);
 
                 return hash.doFinal();
@@ -26,7 +28,7 @@ public class CreatePassword {
         public static boolean isAuthenticated(String sharedKey, byte[] hmac) {
             try {
                 Mac hash = Mac.getInstance("HmacSHA256");
-                SecretKeySpec key = new SecretKeySpec(sharedKey.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+                SecretKeySpec key = new SecretKeySpec(sharedKey.getBytes(StandardCharsets.UTF_8), "HmacSHA1");
                 hash.init(key);
 
                 byte[] result = hash.doFinal();
@@ -37,6 +39,42 @@ public class CreatePassword {
                 throw new RuntimeException(e);
             }
         }
+
+
+        public static byte[] createDigest(String sharedKey, byte[] data) {
+            String sha1 = "";
+            byte[] res = null;
+
+            try {
+                Mac hash = Mac.getInstance("HmacSHA1");
+                SecretKeySpec key = new SecretKeySpec(sharedKey.getBytes(StandardCharsets.UTF_8), "HmacSHA1");
+                hash.init(key);
+                hash.update(data);
+                res = hash.doFinal();
+            }
+
+            catch (NoSuchAlgorithmException | InvalidKeyException e) {
+                e.printStackTrace();
+            }
+
+            return res;
+        }
+
+
+    public static boolean checkAuthenticated(String sharedKey, byte[] data, byte[] hash) {
+        try {
+            Mac sha1 = Mac.getInstance("HmacSHA1");
+            SecretKeySpec key = new SecretKeySpec(sharedKey.getBytes(StandardCharsets.UTF_8), "HmacSHA1");
+            sha1.init(key);
+            sha1.update(data);
+            byte[] result = sha1.doFinal();
+
+            return Arrays.equals(hash, result);
+        }
+        catch (NoSuchAlgorithmException  | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
 
