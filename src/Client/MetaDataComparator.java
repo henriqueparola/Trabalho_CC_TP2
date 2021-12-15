3fca108cb1f2dcbc83d652859d21093bf833ed49
jose <jpdiasfernandes13@gmail.com>
2021-12-15 10:53:56 +0000

    add MetaDataComparator
    - This comparator only compares the last modification date
    
    - If the two names are the same and the creation date is different it
    still doesn't rename the file

---
+++ src/Client/MetaDataComparator.java
@@ -0,0 +1,26 @@
+package Client;
+
+import java.util.Comparator;
+
+public class MetaDataComparator implements Comparator<MetaData> {
+    public  int compare(MetaData mt1, MetaData mt2) {
+        System.out.println("MT1 FILEPATH " + mt1.getFilePath() + "TIME " + mt1.getModified());
+        System.out.println("MT2 FILEPATH " + mt2.getFilePath() + "TIME " + mt2.getModified());
+        if (mt1.getFilePath().equals(mt2.getFilePath()) )
+            if (mt1.getModified() > mt2.getModified()){
+                System.out.println("Res : " + 1);
+                return 1;
+            }
+            else {
+                System.out.println("Res : 0");
+                return 0;
+            }
+        else {
+            System.out.println("Res : " + -1);
+            return -1;
+        }
+
+
+
+}
+}
