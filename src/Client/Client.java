241d3182492730a78e24f423aa37ab56c7805a5d
Parola <parolajc@hotmail.com>
2021-12-21 11:29:05 +0000

    implement FIN packet and modified date in file creation

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,10 +1,24 @@
 package Client;
 
+import Connection.ReliableConnection;
 import Logger.ProtocolLogger;
+import Logger.ProtocolLogger2;
+import Multiplex.ProtocolFrame;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.List;
 import java.util.Map;
 
+/** Represents an Client.
+ * @author Henrique Costa
+ * @author José Pedro
+ * @author Marco Esperança
+ * @version 1.0
+ * @since 1.0
+ */
 public class Client implements Runnable{
     private String[] ipsToSync;
     private String folderToSync;
@@ -43,7 +57,9 @@ public class Client implements Runnable{
         /*Pedido 2 - Conteúdos*/
         Integer numOfFilesToRequest = folderStruct.getStruct().values().stream().mapToInt(List::size).sum();
         Thread[] frthreads = new Thread[numOfFilesToRequest];
+        ProtocolLogger2 pl = ProtocolLogger2.getInstance();
         i = 0;
+        int aux = i;
         for (Map.Entry<String, List<MetaData>> entry : folderStruct.getStruct().entrySet()) {
             for (MetaData metadata : entry.getValue()){
                 frthreads[i] = new Thread(new FileRequest(entry.getKey(),metadata,folderToSync));
@@ -52,12 +68,30 @@ public class Client implements Runnable{
             }
         }
 
+        // special case when I do not have files to request to a ip
+        for (String ip: ipsToSync){
+            if (!folderStruct.getStruct().containsKey(ip)){
+                try {
+                    ReliableConnection rb3 = new ReliableConnection(InetAddress.getByName(ip), 5000);
+                    ProtocolFrame pf3 = new ProtocolFrame((byte) 0x3, 0, null);
+                    pl.loggerInfo("Enviando FIN ao " + ip);
+                    rb3.send(pf3.serialize());
+                    rb3.close();
+                } catch (SocketException e) {
+                    e.printStackTrace();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
         /* Espera pelo recebimento dos ficheiros todos */
         try {
             for (Thread frthread : frthreads) {
                 frthread.join();
             }
-            System.out.println( "> sync "+"\u001B[32m"+ "complete!" + "\u001B[0m");
         }catch (InterruptedException e){
             // TODO
         }
