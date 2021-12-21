be23f4b4da270acd51c19b819b7a26ae27f72bde
Parola <parolajc@hotmail.com>
2021-12-21 11:32:35 +0000

    comment debug statements

--- src/Client/MetaDataComparator.java
+++ src/Client/MetaDataComparator.java
@@ -5,22 +5,22 @@ import java.util.Comparator;
 public class MetaDataComparator implements Comparator<MetaData> {
     public  int compare(MetaData mt1, MetaData mt2) {
         // debug
-        System.out.println("MT1 FILEPATH " + mt1.getFilePath() + "TIME " + mt1.getModified());
-        System.out.println("MT2 FILEPATH " + mt2.getFilePath() + "TIME " + mt2.getModified());
+        //System.out.println("MT1 FILEPATH " + mt1.getFilePath() + "TIME " + mt1.getModified());
+        //System.out.println("MT2 FILEPATH " + mt2.getFilePath() + "TIME " + mt2.getModified());
         if (mt1.getFilePath().equals(mt2.getFilePath()) )
             if (mt1.getModified() > mt2.getModified()){
                 // debug
-                System.out.println("Res : " + 1);
+                //System.out.println("Res : " + 1);
                 return 1;
             }
             else {
                 // debug
-                System.out.println("Res : 0");
+                //System.out.println("Res : 0");
                 return 0;
             }
         else {
             // debug
-            System.out.println("Res : " + -1);
+            //System.out.println("Res : " + -1);
             return -1;
         }
 
