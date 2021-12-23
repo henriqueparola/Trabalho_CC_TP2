e459d608ba9576348a7775089f8adfacf554eef0
Parola <parolajc@hotmail.com>
2021-12-23 00:33:28 +0000

    implement signal to http server

--- src/Server/Http/HttpServer.java
+++ src/Server/Http/HttpServer.java
@@ -8,11 +8,28 @@ import java.net.ServerSocket;
 public class HttpServer implements Runnable{
     FolderStruct fd = FolderStruct.getInstance();
 
+    class ShutDownHttp implements Runnable{
+        ServerSocket ss;
+        public ShutDownHttp(ServerSocket ss){
+            this.ss = ss;
+        }
+
+        @Override
+        public void run() {
+            try {
+                ss.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     @Override
     public void run() {
         boolean running = true;
         try{
             ServerSocket ssocket = new ServerSocket(8080);
+            Runtime.getRuntime().addShutdownHook(new Thread(new ShutDownHttp(ssocket)));
             while(running){
                 ClientHandler ch = new ClientHandler(ssocket.accept());
                 Thread t = new Thread(ch);
@@ -20,7 +37,7 @@ public class HttpServer implements Runnable{
             }
             ssocket.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            //e.printStackTrace();
         }
     }
 }
